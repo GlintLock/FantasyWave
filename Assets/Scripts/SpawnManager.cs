457f2b9d02dc5bc257024a4aaada42d60d4c4952
GlintLock <92694467+GlintLock@users.noreply.github.com>
2023-04-25 11:49:54 -0400

    SpawnWave

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,18 +6,37 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
     private float spawnRange = 0.75f;
+    public int waveNum = 1;
+    public int enemyCount;
     // Start is called before the first frame update
     void Start()
     {
-        float spawnPosX = Random.Range(-spawnRange, spawnRange);
-        float spawnPosY = Random.Range(-spawnRange, spawnRange);
-        Vector2 randomPos = new Vector2(spawnPosX, spawnPosY);
-        Instantiate(enemyPrefab, new Vector2(-0.70f, 0.70f), enemyPrefab.transform.rotation);       
+        SpawnEnemyWave(waveNum);
     }
 
     // Update is called once per frame
     void Update()
     {
-     
+        enemyCount = GameObject.FindObjectsOfType<FollowPlayer>().Length;
+        if (enemyCount == 0)
+        {
+            waveNum++;
+            SpawnEnemyWave(1);
+        } 
+
+    }
+    private Vector2 SpawnEnemy()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosY = Random.Range(-spawnRange, spawnRange);
+        Vector2 randomPos = new Vector2(spawnPosX, spawnPosY);
+        return randomPos;
+    }
+    void SpawnEnemyWave(int numEnemies)
+    {
+        for (int i = 0; i < numEnemies; i++)
+            {
+            Instantiate(enemyPrefab, SpawnEnemy(), enemyPrefab.transform.rotation);
+        }
     }
 }
