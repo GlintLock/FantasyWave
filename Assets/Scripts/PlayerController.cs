dda16e7e7373d0b815d5e6616b4393a4dde2cd0d
GlintLock <seanlgaming@gmail.com>
2023-05-02 08:44:52 -0400

    Added animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,44 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
-
+    Vector2 m_moveInput = Vector2.zero;
     private Rigidbody2D rb;
+    Animator m_animator;
+    SpriteRenderer m_spriteRenderer;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        m_animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
-        float horizontal = 0f;
-        float vertical = 0f;
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            horizontal = -1f;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            horizontal = 1f;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            vertical = -1f;
-        }
-        else if (Input.GetKey(KeyCode.W))
-        {
-            vertical = 1f;
-        }
-
-        Vector2 movement = new Vector2(horizontal, vertical);
-        movement.Normalize(); // make diagonal movement the same speed as horizontal/vertical
-
-        rb.velocity = movement * speed;
+        rb.velocity = m_moveInput * speed;
     }
+
+    private void OnMove(InputValue value)
+    {
+        m_moveInput = value.Get<Vector2>();
+
+        if(m_moveInput != Vector2.zero)
+        {
+            m_animator.SetFloat("xMove", m_moveInput.x);
+            m_animator.SetFloat("yMove", m_moveInput.y);
+        }
+    }
+    
 }
